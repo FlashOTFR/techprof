0d3e9466d481261ed1dfc46ede1a6f842016209a
Randall Burgess <randallallenburgess@gmail.com>
2020-08-02 13:33:15 -0700

    added dynamically generated cards to portfolio page

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import './style.css';
+import Sh1 from '../assets/comicall.png';
 import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 
 const Portfolio = () => {
     const cardInfo = [
-        { image: "", title: "", text: ""},
-        { image: "", title: "", text: ""},
-        { image: "", title: "", text: ""},
-        { image: "", title: "", text: ""},
-        { image: "", title: "", text: ""},
-        { image: "", title: "", text: ""}
+        { image: `${ Sh1 }`, link:"https://chargenproj.herokuapp.com/", title: "D&D Character Sheet Generator", text: "Make your own character sheets for dungeons and dragons quickly!"},
+        { image: `${ Sh1 }`, link:"https://maximilianc88.github.io/ComiCall/", title: "ComiCall", text: "ComiCall!  For comic searches big and small!"},
+        { image: `${ Sh1 }`, link:"https://flashotfr.github.io/Homework5/", title: "Note Taker", text: "Note taking made easy with this app!"},
+        { image: `${ Sh1 }`, link:"https://bigolbudgetboy.herokuapp.com/", title: "Budget Tracker", text: "Keep an eye on your spending with this budget tracker!"},
+        { image: `${ Sh1 }`, link:"https://flashotfr.github.io/Homework3/", title: "Password Generator", text: "Don't want to think up a new password?  Let this app do it for you!"},
+        { image: `${ Sh1 }`, link:"https://flashotfr.github.io/quizlet/", title: "Quiz App", text: "Challenge yourself to get the highest score with this quiz app!"}
     ];
 
+    const renderCard = (card, index) => {
+        return(
+            <Card style={{ width: '18rem' }} key={index}>
+                <Card.Img variant="top" src={card.image} />
+                <Card.Body>
+                    <Card.Title>{card.title}</Card.Title>
+                    <Card.Text>
+                    {card.text}
+                    </Card.Text>
+                    <Button variant="primary" href={card.link}>Go somewhere</Button>
+                </Card.Body>
+            </Card> 
+        )
+    }    
         return ( 
             <div>
                 <div className="container">
@@ -22,18 +37,7 @@ const Portfolio = () => {
                                 <div className="center-text">
                                     <h1>Welcome to my portfolio page! If you are here, you probably have an interest in seeing some of my work! Feel free to peruse some of my projects located here. Or if you prefer, you can check out my github by visiting the contact page!</h1>
                                 </div> 
-
-                                <Card style={{ width: '18rem' }}>
-                                    <Card.Img variant="top" src="holder.js/100px180" />
-                                    <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
-                                        <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                        </Card.Text>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </Card.Body>
-                                    </Card> 
+                                    {cardInfo.map(renderCard)}
                                 
                                 </section>
                             </div>
